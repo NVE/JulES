c5a9fc32b39d99fe62af4d4f923637a2d381825d
culi122 <40186418+cjuli1@users.noreply.github.com>
2023-04-05 10:57:54 +0200

    Comment and clean clearing.jl

--- src/clearing.jl
+++ src/clearing.jl
@@ -1,8 +1,10 @@
+# Initialize market clearing problem and solve for first time step
 function clearing_init(elements, t, clearingdays, cutslocal, nonstoragestateslocal)
     elements1 = copy(elements)
 
-    battery_horizon = SequentialHorizon(clearingdays*12, Hour(2))
-    hydro_horizon = SequentialHorizon(clearingdays, Hour(24)) # TODO: Higher time resolution for PHS
+    # Add horizon to dataelements
+    battery_horizon = SequentialHorizon(clearingdays*12, Hour(2)) # TODO: Replace with user settings
+    hydro_horizon = SequentialHorizon(clearingdays, Hour(24)) # TODO: Higher time resolution for small PHS
     power_horizon = SequentialHorizon(clearingdays*12, Hour(2))
 
     push!(elements1, getelement(COMMODITY_CONCEPT, "BaseCommodity", "Power", 
@@ -12,20 +14,23 @@ function clearing_init(elements, t, clearingdays, cutslocal, nonstoragestatesloc
     push!(elements1, getelement(COMMODITY_CONCEPT, "BaseCommodity", "Battery", 
             (HORIZON_CONCEPT, battery_horizon)))
 
+    # Make modelobjects and add upper slack variable for power production
     modelobjects = getmodelobjects(elements1)
     addPowerUpperSlack!(modelobjects)
 
-    model = Model(HiGHS.Optimizer)
-    set_silent(model)
-    clearing = JuMP_Prob(modelobjects, model)
-    # clearing = HiGHS_Prob(modelobjects)
+    # Build problem from modelobjects and optimizer
+    # model = Model(HiGHS.Optimizer)
+    # clearing = JuMP_Prob(modelobjects, model)
+    clearing = HiGHS_Prob(modelobjects)
 
+    # Set start storages
     shorttermstorages = getshorttermstorages(getobjects(clearing), Hour(10))
     clearingstorages = getstorages(getobjects(clearing))
     longtermstorages = setdiff(clearingstorages, shorttermstorages)
     setstartstoragepercentage!(clearing, shorttermstorages, t, 50)
     setstartstoragepercentage!(clearing, longtermstorages, t, 65)
 
+    # Set end values from cuts generated in stochastic subsystem problems
     clearingendvaluesdict = Dict()
     for cuts in cutslocal
         for (state, value) in cuts.slopes[cuts.cutix] # slopes of last cut
@@ -33,29 +38,35 @@ function clearing_init(elements, t, clearingdays, cutslocal, nonstoragestatesloc
         end
     end
     endvalues = [-clearingendvaluesdict[getid(obj)] for obj in clearingstorages]
-
     clearingendvaluesid = Id(BOUNDARYCONDITION_CONCEPT,"EndValue")
     clearingendvaluesobj = EndValues(clearingendvaluesid, clearingstorages)
     push!(clearing.objects, clearingendvaluesobj)
     updateendvalues!(clearing, clearingendvaluesobj, endvalues)
 
+    # Set outgoing states for non-storage variables
     nonstoragestatesmean = Dict{StateVariableInfo, Float64}()
     for nonstoragestate in keys(nonstoragestateslocal[1])
+        # (id, ix) = getvarout(nonstoragestate)
+        # newnonstoragestate = StateVariableInfo(getvarin(nonstoragestate), (id, ix*2)) # Quick fix different resolution short prognosis (2-hourly) and clearing (hourly)
+        # nonstoragestatesmean[newnonstoragestate] = mean([nonstoragestateslocal[i][nonstoragestate]/2 for i in eachindex(nonstoragestateslocal)])
         nonstoragestatesmean[nonstoragestate] = mean([nonstoragestateslocal[i][nonstoragestate] for i in eachindex(nonstoragestateslocal)])
     end
     setoutgoingstates!(clearing, nonstoragestatesmean)
 
+    # Update and solve problem
     update!(clearing, t)
-
     solve!(clearing)
     
     return clearing, clearingstorages, nonstoragestatesmean, clearingendvaluesdict
 end
 
+# Run market clearing for new time step
 function clearing!(clearing, clearingstorages, startstates, cutslocal, clearingendvaluesdict, nonstoragestateslocal, nonstoragestatesmean, detailedrescopl, enekvglobaldict)
         
+    # Update startstates for all state variables, equals end state of last market clearing
     setstartstates!(clearing, clearingstorages, startstates)
         
+    # Update storage end values from cuts
     for cuts in cutslocal
         for (state, value) in cuts.slopes[cuts.cutix] # slopes of last cut
             clearingendvaluesdict[first(getvarout(state))] = value
@@ -65,23 +76,26 @@ function clearing!(clearing, clearingstorages, startstates, cutslocal, clearinge
     endvalues = [-clearingendvaluesdict[getid(obj)] for obj in clearingstorages]
     updateendvalues!(clearing, clearingendvaluesobj, endvalues)
 
+    # Update end states for non storage variables
     for nonstoragestate in keys(nonstoragestateslocal[1])
         nonstoragestatesmean[nonstoragestate] = mean([nonstoragestateslocal[i][nonstoragestate] for i in eachindex(nonstoragestateslocal)])
     end
     setoutgoingstates!(clearing, nonstoragestatesmean)
 
+    # Update and solve
     update!(clearing, tnormal)
-
     solve!(clearing)
 
+    # Get start states for next iteration
     startstates = getstartstates(clearing, detailedrescopl, enekvglobaldict)
     
     return startstates
 end
 
+# Collect startstates for next iteration. Also calculate for aggregated reservoirs
 function startstates_init(clearing, detailedrescopl, prob, t)
-    # Collect startreservoirs for next iteration. Also for aggregated reservoirs
-    enekvglobaldict = JSON.parsefile("data_fra_dynmodell/magasin_enekvglobal.json") # TODO: 2021 values
+
+    enekvglobaldict = JSON.parsefile("data_fra_dynmodell/magasin_enekvglobal.json") # TODO: only 2021 values, add global enekv as metadata for storages
 
     startstates_ = getstatevariables(clearing.objects)
     getoutgoingstates!(clearing, startstates_)
@@ -122,7 +136,7 @@ function getstartstates(clearing, detailedrescopl, enekvglobaldict)
     getoutgoingstates!(clearing, startstates_)
     
     for var in keys(startstates_)
-        # value = round(startstates_[var], digits=10) # avoid approx 0 negative values
+        # value = round(startstates_[var], digits=10) # avoid approx 0 negative values, ignored by solvers so no problem?
         startstates[getinstancename(first(getvarout(var)))] = startstates_[var]
     end
 
