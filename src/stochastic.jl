2cb31247a689986afd0178b71a476c08001fcdad
culi122 <40186418+cjuli1@users.noreply.github.com>
2023-04-05 10:59:12 +0200

    Comment and clean stochastic.jl

--- src/stochastic.jl
+++ src/stochastic.jl
@@ -1,9 +1,9 @@
-using DistributedArrays
-
+# Make modelobjects for stochastic subsystems, group into subsystems
 function makestochasticobjects(elements, days::Int, offset::Union{Offset,Nothing}, scenario::Int, prices::DArray, short::Bool, master::Bool)
+    
+    # Add horizons to elements
     if short
-        # Add horizons to the dataset
-        battery_horizon = SequentialHorizon(days*24, Hour(1); offset)
+        battery_horizon = SequentialHorizon(days*24, Hour(1); offset) # replace with user settings
         hydro_horizon = SequentialHorizon(days*24, Hour(1); offset)
         power_horizon = SequentialHorizon(days*24, Hour(1); offset)
     else
@@ -24,6 +24,7 @@ function makestochasticobjects(elements, days::Int, offset::Union{Offset,Nothing
     push!(elements, getelement(COMMODITY_CONCEPT, "BaseCommodity", "Battery", 
             (HORIZON_CONCEPT, battery_horizon)))
     
+    # Add prices to elements
     price = prices[scenario]
 
     push!(elements, DataElement(TIMEINDEX_CONCEPT, "RangeTimeIndex", "ShortTermTimeIndex", price["steprange"]))
@@ -41,6 +42,7 @@ function makestochasticobjects(elements, days::Int, offset::Union{Offset,Nothing
             (PRICE_CONCEPT, "Price_" * area)))
     end
 
+    # Make modelobjets from elements and group into subsystems
     modelobjects = getmodelobjects(elements)
     if short
         if master # removes spills from upper and lower storages in PHS, to avoid emptying reservoirs in master problem
@@ -56,22 +58,23 @@ function makestochasticobjects(elements, days::Int, offset::Union{Offset,Nothing
         storagesystems = getstoragesystems_full!(getlongtermstoragesystems(getstoragesystems(modelobjects), Hour(10)))
     end
 
+    # Sort storagesystems
     for storagesystem in storagesystems
         sort!(storagesystem, by = x -> getinstancename(getid(x)))
     end
-
     sort!(storagesystems, by = x -> getinstancename(getid(first(x))))
 
     return storagesystems
 end
 
+# Make master and subproblem objects for each subsystem
 function makemastersubobjects!(inputs, mastersubobjects, shorts)
     (elements, totaldays, numscen, horizonstart, phaseinoffsetdays, prices, short) = inputs
 
     elements1 = copy(elements)
     removeelements!(elements1, short)
 
-    masterobjects = makestochasticobjects(copy(elements1), phaseinoffsetdays, nothing, 1, prices, short, true) # what scenario to use here? use of phasein of scenarios gives similar prices in the start of all scenarios?
+    masterobjects = makestochasticobjects(copy(elements1), phaseinoffsetdays, nothing, 1, prices, short, true) # TODO: what scenario price to use here? random? now 1, use of phasein of scenarios gives similar prices in the start of all scenarios?
 
     subscenarioobjects = []
     for scenario in 1:numscen
@@ -86,7 +89,9 @@ function makemastersubobjects!(inputs, mastersubobjects, shorts)
     end
 end
 
-function removeelements!(elements, short)
+# Aggregate modelobjects and remove modelobjects not relevant for subsystems
+function removeelements!(elements, short) # TODO: Replace with user settings
+    # Aggregate areas
     aggzoneareadict = Dict("NLDBEL" => ["NLD","HUB_NLD","BEL","HUB_BEL"],
     "FRACHE" => ["FRA","CHE"],
     "AUTCZE" => ["AUT","CZE"],
@@ -169,64 +174,17 @@ function initialize_cuts!(modelobjects, cutobjects, maxcuts, lb, numscen)
     return cuts
 end
 
-function getstatevariables(cutobjects::Vector{Any})
-    states = Dict{StateVariableInfo, Float64}()
-    
-    for obj in cutobjects
-        for statevariable in getstatevariables(obj)
-            states[statevariable] = 0.0
-        end
-    end
-    return states
-end
-
-# Set start reservoir as a percentage of capacity
-function setstartstorage!(prob, start, percentage)
-    for obj in prob.objects
-        if obj isa Storage
-
-            dummydelta = MsTimeDelta(Millisecond(0))
-            startreservoir = getparamvalue(getub(obj), start, dummydelta)*percentage/100
-            
-            ingoingstates = Dict{StateVariableInfo, Float64}()
-            for statevariable in getstatevariables(obj)
-                ingoingstates[statevariable] = startreservoir
-            end
-            # display(ingoingstates)
-            
-            setingoingstates!(prob, ingoingstates)
-        end
-    end
-end
-
-function setendstorage!(prob, endtime, percentage)
-    for obj in prob.objects
-        if obj isa Storage
-
-            dummydelta = MsTimeDelta(Millisecond(0))
-            endreservoir = getparamvalue(getub(obj), endtime, dummydelta)*percentage/100
-            
-            outgoingstates = Dict{StateVariableInfo, Float64}()
-            for statevariable in getstatevariables(obj)
-                outgoingstates[statevariable] = endreservoir
-            end
-            # display(outgoingstates)
-            
-            setoutgoingstates!(prob, outgoingstates)
-        end
-    end
-end
-
-# Transfer master problem end reservoir to subproblems (start reservoir)
-function transferboundarystorage!(master, sub, states)
+# Transfer master problem end states to subproblem start states (currently only storage states)
+function transferboundarystates!(master, sub, states)
     states = getoutgoingstates!(master, states)
     # display(states)
     setingoingstates!(sub, states)
 end
 
+# Initialize stochastic subsystem problems and solve for first time step
 function stochastic_init(masterobjects, subobjects, short, storageinfo, numscen, lb, maxcuts, reltol, t)
     @time begin
-        shortstartstorage, medstartstorage, medendvaluesdict = storageinfo
+        shortstartstorage, medstartstorage, medendvaluesdicts = storageinfo
         if short
             startstorage = shortstartstorage
         else
@@ -236,7 +194,6 @@ function stochastic_init(masterobjects, subobjects, short, storageinfo, numscen,
         cutobjects = getcutobjects(masterobjects)
         cuts = initialize_cuts!(masterobjects, cutobjects, maxcuts, lb, numscen);
         states = getstatevariables(cutobjects) # state variables in master and subs for boundary reservoirs
-
 
         # master = JuMP_Prob(masterobjects, Model(HiGHS.Optimizer))
         # subs = [JuMP_Prob(subobject, Model(HiGHS.Optimizer)) for subobject in subobjects] # initialize subproblems
@@ -248,50 +205,84 @@ function stochastic_init(masterobjects, subobjects, short, storageinfo, numscen,
 
         # Update master
         update!(master, t)
-        setstartstorage!(master, t, startstorage)
+        setstartstoragepercentage!(master, getstorages(getobjects(master)), t, startstorage)
 
         # Update subs
         for (i,sub) in enumerate(subs)
             update!(sub, t) # update parameters given problem start time of scenario
 
+            storages = getstorages(getobjects(sub))
             if short
-                setendstorage!(sub, t, startstorage) # set end reservoir
+                setendstoragepercentage!(sub, storages, t, startstorage) # set end reservoir
             else
-                storages = getstorages(getobjects(sub))
                 subendvaluesid = Id(BOUNDARYCONDITION_CONCEPT,"EndValue")
                 subendvaluesobj = EndValues(subendvaluesid, storages)
                 push!(sub.objects, subendvaluesobj)
-                subendvalues = [medendvaluesdict[getinstancename(getid(obj))] for obj in storages]
+                subendvalues = [medendvaluesdicts[i][getinstancename(getid(obj))] for obj in storages]
                 updateendvalues!(sub, subendvaluesobj, subendvalues)
             end
         end
 
         ub = 0
-        while !((abs((ub-lb)/ub) < reltol) || abs(ub-lb) < 1)
-            solve!(master)
-            lb = getvarvalue(master, getfuturecostvarid(cuts),1)
-#             display(lb)
-            ub = 0
-            for (i,sub) in enumerate(subs)
-
-                transferboundarystorage!(master, sub, states)
-
-                solve!(sub)
-
-                ub += getobjectivevalue(sub)
-                cutparameters[i] = getcutparameters(sub, states)
-            end
-            ub /= numscen
-            updatecuts!(master, cuts, cutparameters)
-#             display(ub)
-#             display(abs((lb-ub)/lb))
-#             display(cuts.slopes)
-        end
+        cutreuse = false
+        iterate_convergence!(master, subs, cuts, cutparameters, states, numscen, cutreuse, lb, ub, reltol)
 
         return (master, subs, states, cuts)
     end
 end
 
+# Iterate until convergence between master and subproblems
+function iterate_convergence!(master, subs, cuts, cutparameters, states, numscen, cutreuse, lb, ub, reltol)
+    count = 0
+
+    while !((abs((ub-lb)/ub) < reltol) || abs(ub-lb) < 1)
+
+        if (cutreuse && count == 0) # try to reuse cuts from last iteration
+            try
+                solve!(master)
+            catch
+                println("Retrying first iteration without cuts")
+                clearcuts!(master, cuts)
+                solve!(master)
+                cutreuse = false
+            end
+        elseif cutreuse
+            try
+                solve!(master)
+            catch
+                println("Restarting iterations without cuts")
+                cutreuse && clearcuts!(master, cuts)
+                solve!(master)
+                cutreuse = false
+            end
+        else
+            solve!(master)
+        end
+
+        lb = getvarvalue(master, getfuturecostvarid(cuts),1)
+        ub = 0
+        
+        for (i,sub) in enumerate(subs)
+
+            transferboundarystates!(master, sub, states)
+            
+            solve!(sub)
+
+            ub += getobjectivevalue(sub)
+            cutparameters[i] = getcutparameters(sub, states)
+        end
+        ub /= numscen
+
+        count += 1
+        (count == 1 && cutreuse) && clearcuts!(master, cuts) # reuse cuts in first iteration
+        updatecuts!(master, cuts, cutparameters)
+#             display(ub)
+#             display(abs((lb-ub)/lb))
+#             display(cuts.slopes)
+    end
+end
+
+# Initialize stochastic subsystem problems in parallel
 function pl_stochastic_init!(numcores, storagesystemobjects, shorts, masters_, subs_, states_, cuts_, storageinfo, numscen, lb, maxcuts, reltol, t)
     @sync @distributed for core in 1:(numcores-1)
         storagesystemobject = localpart(storagesystemobjects)
@@ -312,19 +303,21 @@ function pl_stochastic_init!(numcores, storagesystemobjects, shorts, masters_, s
     end
 end
 
+# Update prices in exogen prices
 function updatestochasticprices!(prob::Prob, prices::Vector{Dict}, scenario::Int)
     price = prices[scenario]
     for obj in prob.objects
         if obj isa ExogenBalance
             priceix = findfirst(x -> x == split(getinstancename(getid(obj)), "PowerBalance_")[2], price["names"])
             
-            obj.price.param.profile.index = price["steprange"]
+            obj.price.param.profile.index = price["steprange"] # TODO: API function in TuLiPa
             obj.price.param.profile.values .= price["matrix"][:, priceix]
         end
     end
 end
 
-@everywhere function stochastic!(master, subs, states, cuts, startstates, medprices, shortprices, medendvaluesdict, short, numscen, reltol, t)
+# Run stochastic subsystem problem
+@everywhere function stochastic!(master, subs, states, cuts, startstates, medprices, shortprices, medendvaluesdicts, short, numscen, reltol, t)
         
     # Init cutparameters
     cutparameters = Vector{Tuple{Float64, Dict{StateVariableInfo, Float64}}}(undef, length(subs)) # preallocate for cutparameters from subproblems
@@ -347,7 +340,7 @@ end
             updatestochasticprices!(sub, shortprices, i) 
         else
             subendvaluesobj = sub.objects[findfirst(x -> getid(x) == Id(BOUNDARYCONDITION_CONCEPT,"EndValue"), sub.objects)]
-            subendvalues = [medendvaluesdict[getinstancename(getid(obj))] for obj in substorages]
+            subendvalues = [medendvaluesdicts[i][getinstancename(getid(obj))] for obj in substorages]
             updateendvalues!(sub, subendvaluesobj, subendvalues)
             updatestochasticprices!(sub, medprices, i) 
         end
@@ -357,56 +350,12 @@ end
 
     lb = cuts.lower_bound
     ub = 0
-
-    count = 0
     cutreuse = true
-    while !((abs((ub-lb)/ub) < reltol) || abs(ub-lb) < 1)
-
-        # TODO: Other way to fix this?
-        if count == 0 # try to reuse cuts from last iteration
-            try
-                solve!(master)
-            catch
-                println("Retrying first iteration without cuts")
-                clearcuts!(master, cuts)
-                solve!(master)
-                cutreuse = false
-            end
-        else
-            try
-                solve!(master)
-            catch
-                println("Restarting iterations without cuts")
-                cutreuse && clearcuts!(master, cuts)
-                solve!(master)
-                cutreuse = false
-            end
-        end
-
-        lb = getvarvalue(master, getfuturecostvarid(cuts),1)
-        ub = 0
-        
-        for (i,sub) in enumerate(subs)
-
-            transferboundarystorage!(master, sub, states)
-            
-            solve!(sub)
-
-            ub += getobjectivevalue(sub)
-            cutparameters[i] = getcutparameters(sub, states)
-        end
-        ub /= numscen
-
-        count += 1
-        count == 1 && clearcuts!(master, cuts) # reuse cuts in first iteration
-        updatecuts!(master, cuts, cutparameters)
-#             display(ub)
-#             display(abs((lb-ub)/lb))
-#             display(cuts.slopes)
-    end
+    iterate_convergence!(master, subs, cuts, cutparameters, states, numscen, cutreuse, lb, ub, reltol)
 end
 
-function pl_stochastic!(numcores, masters_, subs_, states_, cuts_, startstates, medprices, shortprices, medendvaluesdict, shorts, numscen, reltol, t, skipmed)
+# Run stochastic subsystem problems in parallel
+function pl_stochastic!(numcores, masters_, subs_, states_, cuts_, startstates, medprices, shortprices, medendvaluesdicts, shorts, numscen, reltol, t, skipmed)
     @sync @distributed for core in 1:(numcores-1)
         masters = localpart(masters_)
         subs = localpart(subs_)
@@ -419,14 +368,60 @@ function pl_stochastic!(numcores, masters_, subs_, states_, cuts_, startstates, 
             for ix in range
                 localix += 1
                 if !(!short[localix] && (skipmed.value > 0))
-                    stochastic!(masters[localix], subs[localix], states[localix], cuts[localix], startstates, medprices, shortprices, medendvaluesdict, short[localix], numscen, reltol, t)
+                    stochastic!(masters[localix], subs[localix], states[localix], cuts[localix], startstates, medprices, shortprices, medendvaluesdicts, short[localix], numscen, reltol, t)
                 end
             end
         end
         if skipmed.value == 0
-            GC.gc() # regulary garbage collection
+            GC.gc() # regulary garbage collection, else there is always one processor core for each time step that the others have to wait for
         end
     end
+end
+
+# Distribute subsystems after number of objects in subsystem, equal amount of subsystems on each core
+# Consist of several elements:
+# - Testsort to see how an array of this size would be distributed by default by DistributedArrays
+# - Make rules for how to distribute subsystems 
+#   - Example: 6 elements on 3 cores, assume it is already sorted by most modelobjects in subsystems
+#   - Should distribute: 
+#       -  Core 1: Index 1 and 6
+#       -  Core 2: Index 2 and 5
+#       -  Core 3: Index 3 and 4
+# TODO: Replace with low level Distributed functions that are more flexible than DistributedArrays
+function distribute_subsystems(ustoragesystemobjects::Vector, ushorts::Vector)
+    objectcount = [length(first(objs)) for objs in ustoragesystemobjects]
+    sortedindex = sortperm(objectcount, rev=true)
+    testsort = distribute(collect(1:length(ustoragesystemobjects)))
+    storagesystemobjects = Vector(undef,length(objectcount))
+    shorts = Vector(undef,length(ushorts))
+    direction = 1
+    rangeix = 1
+    localix = 1
+    for i in 1:length(objectcount)
+        (range,) = testsort.indices[rangeix]
+        if localix > length(range) # possible for last localix
+            rangeix = 1
+            direction = 1
+            (range,) = testsort.indices[rangeix]
+        end
+
+        storagesystemobjects[collect(range)[localix]] = ustoragesystemobjects[sortedindex][i]
+        shorts[collect(range)[localix]] = ushorts[sortedindex][i]
+        rangeix += direction
+
+        if rangeix > length(testsort.indices)
+            direction = -1
+            rangeix += direction
+            localix += 1
+        elseif rangeix == 0
+            direction = 1
+            rangeix += direction
+            localix += 1
+        end
+    end
+    storagesystemobjects = distribute(storagesystemobjects)
+    shorts = distribute(shorts, storagesystemobjects)
+    return storagesystemobjects, shorts
 end
 
 
