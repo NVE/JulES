fed173ef9346f4c35aae68c7938aee001e73ab8b
Harald <haen@nve.no>
2024-03-07 11:42:09 +0100

    many changes

--- src/io.jl
+++ src/io.jl
@@ -1,10 +1,9 @@
-
+"""
+Definition of default input and output types
+"""
 
 
 struct DefaultJulESInput <: AbstractJulESInput
-    pp_dist::Dict
-    ev_dist::Dict
-    sp_dist::Dict
     # TODO: complete
 
     function DefaultJulESInput(dataset, config)
@@ -17,10 +16,8 @@ struct DefaultJulESInput <: AbstractJulESInput
         subsystems = getsubsystems(dataset, config, horizons)::Dict{String, Vector{DataElement}}
 
         # TODO: use throw-away-dummy-modelobjs for data proc
-        pp_dist = distribute_pp(dataset, config)
-        ev_dist = distribute_ev(dataset, config)
 
-        return new(pp_dist, ev_dist)
+        return new()
     end
 end
 
@@ -28,22 +25,15 @@ struct DefaultJulESOutput <: AbstractJulESOutput
     # TODO: complete
 end
 
+# TODO: complete
 get_cores(input) = nothing
 get_horizons(input) = nothing
 get_simulation_period(input) = nothing
-get_startstates_pp(input) = nothing
+get_startstates_ppp(input) = nothing
 
 
-"""
-How price prognosis (pp) problems are distributed on cores initially
-"""
-function get_pp_dist(input::DefaultJulESInput) end
 
-function get_ev_dist(input::DefaultJulESInput) end
-
-function get_sp_dist(input::DefaultJulESInput) end
-
-function get_mp_dist(input::DefaultJulESInput) end
-
-function get_cp_core(input::DefaultJulESInput) end 
+# Should live here and not in slot in PricePrognosisProblem?
+function get_medendvaluesdict(input)
+end
 
