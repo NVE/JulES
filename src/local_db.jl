63a0c4877658bb7a5a16c2f3f1ec6b6f9468cda5
Harald <haen@nve.no>
2024-03-07 11:41:47 +0100

    doc and types

--- src/local_db.jl
+++ src/local_db.jl
@@ -1,31 +1,61 @@
+"""
+In-memory-process-local-database for JulES.
 
-# Define simple local-database API 
+We utilize some of Julia's reflection (fieldnames, getfield) 
+and metaprogramming (eval, Expr) capabilities to implement 
+a database object in the global scope of a Julia process.
+We leared this technique from this package: 
+https://github.com/ChrisRackauckas/ParallelDataTransfer.jl
+
+The database is just a struct with slots for all the different
+things we want to store on cores while running JulES. 
+    
+Some slots hold read-only data that is identical on all cores (input)
+
+Some slots hold stateful objects that are managed on a particular core
+while other cores hold synced copies (horizons)
+
+Some slots hold optimization problems where a problem only resides
+at exactly one core at a time, but may be moved to antother core 
+over time (ppp, evp, mp, sp, cp)
+
+Some slots hold info about on which cores problems are stores. This
+enables JulES to locate and use any problem from any core, which is
+very useful eg. when solving stochastic optimization problems where
+the masterproblem and the different subproblems reside on different
+cores, or eg. when we need to get results from a problem residing on
+one core in order to transfer it and give it as input to another problem 
+residing on a different core (ppp_dist, evp_dist, mp_dist, sp_dist, cp_core)
+
+Many of the slots contain timing data.
+"""
 
 const _LOCAL_DB_NAME = :_local_db
 
-# TODO: Complete this
+# TODO: Complete this (add more timings and maybe other stuff)
 mutable struct LocalDB
-    input
-    horizon
+    input::AbstractJulESInput
+    horizons::Dict{ScenarioTermCommodity, Horizon}
 
-    pp
-    ev
-    mp
-    sp
-    cp
+    ppp::Dict{Scenario, PricePrognosisProblem}
+    evp::Dict{ScenarioSubsystem, EndValueProblem}
+    sp::Dict{ScenarioSubsystem, ScenarioProblem}
+    mp::Dict{Subsystem, MasterProblem}
+    cp::Union{Nothing, ClearingProblem}
 
-    pp_dist
-    ev_dist
-    mp_dist
-    sp_dist
-    cp_core
+    ppp_dist::Vector{ScenarioCore}
+    evp_dist::Vector{ScenarioSubsystemCore}
+    mp_dist::Vector{SubsystemCore}
+    sp_dist::Vector{ScenarioSubsystemCore}
+    cp_core::Int
 
-    cp_time_solve
-    cp_time_update
-    cp_time_cuts
-    cp_time_startstates
-    cp_time_endstates
+    cp_time_solve::Float64
+    cp_time_update::Float64
+    cp_time_cuts::Float64
+    cp_time_startstates::Float64
+    cp_time_endstates::Float64
 
+    # TODO: Fix by handle types, will fail now
     function LocalDB()
         n = length(fieldnames(typeof(LocalDB)))
         return new([nothing for _ in 1:n]...)
@@ -37,7 +67,7 @@ function create_local_db()
         db = getfield(Main, _LOCAL_DB_NAME)
         isnothing(db) || error("$_LOCAL_DB_NAME already exists")
     end
-    Core.eval(Main, Expr(:(=), _LOCAL_DB_NAME, LocalDB())
+    Core.eval(Main, Expr(:(=), _LOCAL_DB_NAME, LocalDB()))
     return
 end
 
