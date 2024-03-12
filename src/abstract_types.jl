73383c2bdb5a2b31c94aac03c372d9e261f44085
Harald <haen@nve.no>
2024-03-12 20:59:13 +0100

    Comment ScenarioModellingMethod

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -1,4 +1,6 @@
 abstract type AbstractJulESInput end
 abstract type AbstractJulESOutput end
 abstract type AbstractScenario end
+
+# TODO: Rename to AbstractScenarioModellingMethod?
 abstract type ScenarioModellingMethod end
