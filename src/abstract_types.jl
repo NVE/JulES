82aa087883bd708f0d34111efafca14fbf9541d8
Harald <haen@nve.no>
2024-05-29 12:50:35 +0200

    Added interface

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -3,7 +3,12 @@ abstract type AbstractJulESOutput end
 abstract type AbstractScenario end
 abstract type AbstractSubsystem end
 abstract type AbstractScenarioModellingMethod end
+
+# TODO: Update code to return and store S for output reporting
+"""
+Interface: 
+estimate_S0(m::AbstractInflowModel, t::ProbTime) -> S0 where S0::Vector{Float64}
+predict(m::AbstractInflowModel, S0::::Vector{Float64}, t::ProbTime) -> (Q, S) where Q::Vector{Float64} and S::Vector{Vector{Float64}}
+"""
 abstract type AbstractInflowModel end
-
-
 const ABSTRACT_INFLOW_MODEL = "AbstractInflowModel"
