f9566b6c1a2e22efb8c684b6dff8535abdb5d884
Harald <haen@nve.no>
2024-05-31 09:56:21 +0200

    Changed AbstractInflowModel interface

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -6,11 +6,9 @@ abstract type AbstractScenarioModellingMethod end
 
 """
 Interface:
-    update(m::AbstractInflowModel, t::ProbTime)
+    estimate_u0(m::AbstractInflowModel, t::ProbTime) -> u0::Vector{Float64}
 
-    estimate_u0(m::AbstractInflowModel) -> u0::Vector{Float64}
-
-    predict(m::AbstractInflowModel, u0::::Vector{Float64}) -> (Q, u) 
+    predict(m::AbstractInflowModel, u0::::Vector{Float64}, t::ProbTime) -> (Q, u) 
         where Q::Vector{Float64} and u::Vector{Vector{Float64}}
 """
 abstract type AbstractInflowModel end
