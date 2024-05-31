00c957d0248976b6eebd26b943674f773320125c
Harald <haen@nve.no>
2024-05-31 08:40:41 +0200

    AbstractInflowModel

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -3,3 +3,15 @@ abstract type AbstractJulESOutput end
 abstract type AbstractScenario end
 abstract type AbstractSubsystem end
 abstract type AbstractScenarioModellingMethod end
+
+"""
+Interface:
+    update(m::AbstractInflowModel, t::ProbTime)
+
+    estimate_u0(m::AbstractInflowModel) -> u0::Vector{Float64}
+
+    predict(m::AbstractInflowModel, u0::::Vector{Float64}) -> (Q, u) 
+        where Q::Vector{Float64} and u::Vector{Vector{Float64}}
+"""
+abstract type AbstractInflowModel end
+const ABSTRACT_INFLOW_MODEL = "AbstractInflowModel"
