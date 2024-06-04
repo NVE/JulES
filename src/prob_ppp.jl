6280189191ee2fc747fcc391baa91fdb05ab0016
Harald <haen@nve.no>
2024-06-04 09:24:42 +0200

    make_obj

--- src/prob_ppp.jl
+++ src/prob_ppp.jl
@@ -72,8 +72,10 @@ function create_ppp(db::LocalDB, scenix::Int)
     return
 end
 
-function make_obj(elements::Vector{DataElement}, hydro_horizon::Horizon, power_horizon::Horizon; validate::Bool=false)
-    elements1 = copy(elements)
+function make_obj(elements::Vector{DataElement}, hydro_horizon::Horizon, power_horizon::Horizon, scenix::Int, iprogtype::String, ifm_replacemap; validate::Bool=false)
+    elements1 = copy_elements_iprogtype(elements, iprogtype, ifm_replacemap)
+
+    add_scenix_to_InflowParam(elements1, scenix)
 
     set_horizon!(elements1, "Power", power_horizon)
     set_horizon!(elements1, "Battery", power_horizon)
@@ -83,6 +85,7 @@ function make_obj(elements::Vector{DataElement}, hydro_horizon::Horizon, power_h
 
     return modelobjects
 end
+
 
 function simplify!(modelobjects::Dict; aggzone::Dict=Dict(), removestartup::Bool=true, removetransmissionramping::Bool=true, aggsupplyn::Int=0, removestoragehours::Int=0, residualarealist::Vector=[])
     # Aggregate price areas and add power balance slack variables
