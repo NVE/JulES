d4472117d4cee9f04665a5ec72d8975f06f55d91
culi122 <40186418+cjuli1@users.noreply.github.com>
2024-06-28 12:59:18 +0200

    Add ifm to demo and fix memory

--- src/io.jl
+++ src/io.jl
@@ -798,7 +798,7 @@ function get_output_memory(output)
     settings = get_settings(db)
 
     if settings["results"]["memory"]
-        names = ["coreid", "sum_unique", "core", "input", "output", "horizons", "dummyobjects", "dummyobjects_ppp", "startstates", "subsystems", "subsystems_evp", "subsystems_stoch", "scenmod_sim", "scenmod_stoch", "ppp", "prices_ppp", "evp", "mp", "sp", "cp", "dist_ppp", "dist_evp", "dist_mp", "dist_sp", "core_cp", "div"]
+        names = ["coreid", "sum_unique", "core", "input", "output", "horizons", "dummyobjects", "dummyobjects_ppp", "startstates", "subsystems", "subsystems_evp", "subsystems_stoch", "scenmod_sim", "scenmod_stoch", "ifm", "ppp", "prices_ppp", "evp", "mp", "sp", "cp", "dist_ifm", "dist_ppp", "dist_evp", "dist_mp", "dist_sp", "core_cp", "div", "ifm_output", "ifm_derived"]
         df = DataFrame(DataFrame([[] for _ = names] , names))
         cores = get_cores(db)
         for core in cores # TODO: Do sync
